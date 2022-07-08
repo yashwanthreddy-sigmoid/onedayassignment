b16ce439bdc90bb29754dc977a10e0ebadc20a35
yashwanthreddy-sigmoid <98736166+yashwanthreddy-sigmoid@users.noreply.github.com>
2022-07-08 13:18:51 +0530

    Update companies.py

--- onedayproject/companies.py
+++ onedayproject/companies.py
@@ -5,8 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 url = "https://stock-market-data.p.rapidapi.com/stock/historical-prices"
-CompanyName=["AAPL", "ABNB", "ADBE", "ADI", "ADP", "ADSK", "AEP", "ALGN", "AMAT", "AMD", "AMGN", "AMZN", "ANSS", "ASML", "ATVI", "AVGO", "AZN", "BIDU", "BIIB", "BKNG", "CDNS", "CEG", "CHTR", "CMCSA", "COST", "CPRT", "CRWD", "CSCO", "CSX", "CTAS", "CTSH", "DDOG", "DLTR", "DOCU", "DXCM", "EA", "EBAY", "EXC", "FAST", "FISV", "FTNT", "GILD", "GOOG", "GOOGL", "HON", "IDXX", "ILMN", "INTC", "INTU", "ISRG", "JD", "KDP", "KHC", "KLAC", "LCID", "LRCX", "LULU", "MAR", "MCHP", "MDLZ", "MELI", "META", "MNST", "MRNA", "MRVL", "MSFT", "MTCH", "MU", "NFLX", "NTES", "NVDA", "NXPI", "ODFL", "OKTA", "ORLY", "PANW", "PAYX", "PCAR", "PDD", "PEP", "PYPL", "QCOM", "REGN", "ROST", "SBUX", "SGEN", "SIRI", "SNPS", "SPLK", "SWKS", "TEAM", "TMUS", "TSLA", "TXN", "VRSK", "VRSN", "VRTX", "WBA", "WDAY", "XEL", "ZM", "ZS"]
-
 CompanyName1=["AAPL", "ABNB", "ADBE", "ADI", "ADP", "ADSK", "AEP", "ALGN", "AMAT", "AMD", "AMGN", "AMZN", "ANSS","ASML"]
 for company in CompanyName1:
    querystring = {"ticker_symbol": company, "years": "5", "format": "json"}
